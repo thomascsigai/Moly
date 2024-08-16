0a39ec44ccfa653df76add734733290c395755e1
Thomas Csigai <thomascsigai1@gmail.com>
2024-08-16 11:51:38 +0200

    Adding cube  vertices

--- Moly/src/Renderer/Vertices.h
+++ Moly/src/Renderer/Vertices.h
@@ -6,3 +6,13 @@
     vec.push_back({ glm::vec3(-0.5f, -0.5f, 0.0f) }); \
     vec.push_back({ glm::vec3( 0.5f, -0.5f, 0.0f) }); \
     vec.push_back({ glm::vec3( 0.0f,  0.5f, 0.0f) });
+
+#define MOLY_DEFINE_CUBE_VERTICES(vec)            \
+    vec.push_back({ glm::vec3(-0.5, -0.5,  0.5) }); \
+    vec.push_back({ glm::vec3( 0.5, -0.5,  0.5) }); \
+    vec.push_back({ glm::vec3(-0.5,  0.5,  0.5) }); \
+    vec.push_back({ glm::vec3( 0.5,  0.5,  0.5) }); \
+    vec.push_back({ glm::vec3(-0.5, -0.5, -0.5) }); \
+    vec.push_back({ glm::vec3( 0.5, -0.5, -0.5) }); \
+    vec.push_back({ glm::vec3(-0.5,  0.5, -0.5) }); \
+    vec.push_back({ glm::vec3( 0.5,  0.5, -0.5) });
